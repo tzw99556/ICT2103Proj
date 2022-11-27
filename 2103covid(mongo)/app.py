065cfe97703862a33fda8b9e9cfeb8b6486aed56
tzw99556 <90042944+tzw99556@users.noreply.github.com>
2022-11-28 00:11:49 +0800

    MongoDB left 4-5 more queries
    MongoDB left 4-5 more queries

--- 2103covid(mongo)/app.py
+++ 2103covid(mongo)/app.py
@@ -30,7 +30,7 @@ app = Flask('2103proj')
 #for this , you if its . find means find everything in collection you can replace it with 
 #insertmany , insert one depending on the mongo query you need. 
 
-#query 7 aggregate always use this format
+#query 11 aggregate always use this format
 personfullyvaccinated = mycol.aggregate( [{
     "$group" : 
         {"_id" : "", 
@@ -38,18 +38,122 @@ personfullyvaccinated = mycol.aggregate( [{
          }}
     ])
 
+# query 2
+totalnumberofcasestodate = mycol1.aggregate(
+        [{ "$group": 
+        {"_id": '$location',
+        "Total_cases":{"$max":'$total_cases'},"data":{"$last": '$date'}
+
+        } 
+        
+        
+        }])
+
+#query 10
+totalvaccineSEA = mycol.find({},  {"_id": 0, "COUNTRY": 1,"PERSONS_FULLY_VACCINATED": 1})
+
+#query 8
+percentagetotaldeath = mycol1.aggregate(
+         [{ "$group": 
+        {"_id": '$location',
+        "Total_Cases":{"$last":'$total_cases'},
+        "Total Deaths":{"$last": "$total_deaths"},
+        "Latest Date": {"$last": "$date"}
+
+        } 
+        
+        
+        }])
+
+
+#query 9
+casesanddeathwithinpopulation = mycol1.aggregate([{ "$group": 
+        {"_id": '$location',
+        "Total Cases":{"$last":'$total_cases'},
+        "Total Deaths":{"$last": "$total_deaths"},
+        "Population": {"$max": "$population"},
+        "Date":{"$last": "$date"}
+
+        } 
+        
+        
+        }])
+
+#query 6
+totalcase = mycol1.aggregate(
+    [{
+        "$group":{"_id": "$location", "total": {"$last": "$total_cases"}}},{"$group":{"_id": "null", "TotalCase": {"$sum": "$total"}}}])
+
+
+
+
+
 
 #mongo db 
 @app.route('/')
 def showData():
         #3rd , declare a variable to store the queries you want to find 
+        #11 query
         vaccinatedtotal={}
         vaccinatedtotal= personfullyvaccinated 
+
+        #2nd query
+       # totalcases = list()
+        
+        totalcaseslist = []
+        totalcasesnumber = []
+        # make the dictionary to list so can display in chartjs
+        for i in totalnumberofcasestodate:
+                totalcaseslist.append(i['_id'])
+       
+                totalcasesnumber.append(i['Total_cases'])
+
+        #10 query 
+        countrypeoplevaccinated =[]
+        countryname =[]
+        for x in totalvaccineSEA:
+                countryname.append(x['COUNTRY'])
+                countrypeoplevaccinated.append(x['PERSONS_FULLY_VACCINATED'])
+        #1st query 
+        totalconfirmedcase={}
+        totalconfirmedcase=  totalcase
+
         
         #return render template to index html. and store the variable in the variable so in the html you can call the variable to display it. 
-        return render_template('index.html', vaccinatedtotal = vaccinatedtotal)
+        return render_template('index.html', vaccinatedtotal = vaccinatedtotal, totalcaseslist =totalcaseslist, totalcasesnumber=totalcasesnumber,
+        countrypeoplevaccinated = countrypeoplevaccinated , countryname = countryname, totalconfirmedcase = totalconfirmedcase
+        )
        
-  
+@app.route('/secondpage')
+def secondpage():
+          #10 query 
+        totaldeath =[]
+        totalcases =[]
+        location = []
+        for x in percentagetotaldeath:
+                totaldeath.append(x['Total Deaths'])
+                totalcases.append(x['Total_Cases'])
+                location.append(x['_id'])
+
+
+        #9 query 
+        totaldeathwithinpop = []
+        totalcaseswithinpop = []
+        population =[]
+        countryname = []
+        for y in casesanddeathwithinpopulation:
+                totalcaseswithinpop.append(y['Total Cases'])
+                totaldeathwithinpop.append(y['Total Deaths'])
+                population.append(y['Population'])
+                countryname.append(y['_id'])
+
+
+        return render_template(
+                "secondpage.html", totalcases=totalcases, totaldeath=totaldeath, location=location,
+        totalcaseswithinpop=totalcaseswithinpop , totaldeathwithinpop =totaldeathwithinpop , population = population, 
+        countryname = countryname
+        
+        )
 
 if __name__ == '__main__':
     app.config["TEMPLATES_AUTO_RELOAD"] = True
